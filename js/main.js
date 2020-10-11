f14bcb5fe9654c952bbcf6f33c283fee5376b370
Max <maximlev01234@gmail.com>
2020-10-11 20:11:06 +0300

    added lame animation to site

--- js/main.js
+++ js/main.js
@@ -20,3 +20,35 @@ function toggleMenu() {
 
     }
 }
+
+
+
+let start = Date.now(); // remember start time
+
+let timer = setInterval(function() {
+    // how much time passed from the start?
+    let timePassed = Date.now() - start;
+
+    if (timePassed >= 2000) {
+        clearInterval(timer); // finish the animation after 2 seconds
+        return;
+    }
+
+    // draw the animation at the moment timePassed
+    draw(timePassed);
+
+}, 20);
+
+// as timePassed goes from 0 to 2000
+// left gets values from 0px to 400px
+const selects = document.getElementsByClassName("select")
+
+for (let counter = 0; counter < selects.length; counter++) {
+    selects[counter].style.position = 'relative';
+}
+
+function draw(timePassed) {
+    for (let counter = 0; counter < selects.length; counter++) {
+        selects[counter].style.left = 99 - timePassed / 20 + 'px';
+    }
+}
