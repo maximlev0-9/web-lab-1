08d46a1c414c85089ea34f7e9bc91ebd7a23e351
Max <maximlev01234@gmail.com>
2020-10-12 11:47:44 +0300

    removed lame js animation and added animations to images in *featured-animations*

--- js/main.js
+++ js/main.js
@@ -23,32 +23,32 @@ function toggleMenu() {
 
 
 
-let start = Date.now(); // remember start time
+// let start = Date.now(); // remember start time
 
-let timer = setInterval(function() {
-    // how much time passed from the start?
-    let timePassed = Date.now() - start;
+// let timer = setInterval(function() {
+//     // how much time passed from the start?
+//     let timePassed = Date.now() - start;
 
-    if (timePassed >= 2000) {
-        clearInterval(timer); // finish the animation after 2 seconds
-        return;
-    }
+//     if (timePassed >= 2000) {
+//         clearInterval(timer); // finish the animation after 2 seconds
+//         return;
+//     }
 
-    // draw the animation at the moment timePassed
-    draw(timePassed);
+//     // draw the animation at the moment timePassed
+//     draw(timePassed);
 
-}, 20);
+// }, 20);
 
-// as timePassed goes from 0 to 2000
-// left gets values from 0px to 400px
-const selects = document.getElementsByClassName("select")
+// // as timePassed goes from 0 to 2000
+// // left gets values from 0px to 400px
+// const selects = document.getElementsByClassName("select")
 
-for (let counter = 0; counter < selects.length; counter++) {
-    selects[counter].style.position = 'relative';
-}
+// for (let counter = 0; counter < selects.length; counter++) {
+//     selects[counter].style.position = 'relative';
+// }
 
-function draw(timePassed) {
-    for (let counter = 0; counter < selects.length; counter++) {
-        selects[counter].style.left = 99 - timePassed / 20 + 'px';
-    }
-}
+// function draw(timePassed) {
+//     for (let counter = 0; counter < selects.length; counter++) {
+//         selects[counter].style.left = 99 - timePassed / 20 + 'px';
+//     }
+// }
